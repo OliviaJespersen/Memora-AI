e5e159e9b9806444a185d8e90098806d2b8eb382
OliviaJespersen <olivia.famjespersen@gmail.com>
2024-10-19 14:28:20 +0200

    Made the search ignore capitalization

--- search_engine.py
+++ search_engine.py
@@ -7,4 +7,4 @@ class SearchEngine:
 
 
     def search(self, query, dataset):
-        return [file_name for file_name in dataset if fuzz.partial_token_set_ratio(query, dataset[file_name]["image_text"] + dataset[file_name]["tags"]) >= self.threshold]
+        return [file_name for file_name in dataset if fuzz.partial_token_set_ratio(query.lower(), (dataset[file_name]["image_text"] + dataset[file_name]["tags"]).lower()) >= self.threshold]
